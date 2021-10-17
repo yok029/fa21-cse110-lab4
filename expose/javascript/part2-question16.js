9eb8791832bee517feafd54221f8b35197b47b16
yok029 <yok029@ucsd.edu>
2021-10-17 14:21:43 -0700

    commit

---
+++ expose/javascript/part2-question16.js
@@ -0,0 +1,19 @@
+let statistics={
+    redCars: 21,
+    blueCars: 45,
+    greenCars: 12,
+    raceCars: 5,
+    blackCars: 50,
+    rareCars: 2
+};
+
+    for(const prop in statistics)
+    {
+        if(prop[0] =='r' || statistics[prop]%2==1)
+        {
+            console.log(statistics[prop]);
+        }
+          
+    }
+    
+
---
+++ expose/javascript/part2-question18.js
@@ -0,0 +1,10 @@
+
+setInterval(runfunction,1000);
+function runfunction()
+{
+let d=new Date();
+let time=d.toLocaleTimeString();
+console.log(time);
+}
+
+
